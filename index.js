23966ea08a7983eb08e262a6e1265bf560649efe
Riccardo Cecchini <riccardo@leonardotravel.it>
2016-08-12 16:32:54 +0200

    Implemented callbacks =)
    When you call any .NET method, if you set the last argument as function
    it will be used as callback.
    
    DotNetObject.Method(arg1, arg2, function(response, err){
    //Read response here
    });

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 /*
 
-Fusion.js - Handle .NET objects in JavaScript
-Copyright (C) 2016 Riccardo Cecchini (https://github.com/cekkr)
+ Fusion.js - Handle .NET objects in JavaScript
+ Copyright (C) 2016 Riccardo Cecchini (https://github.com/cekkr)
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 3 of the License.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
-*/
+ */
 
 var reflect = require('harmony-reflect');
 var net = require('net');
@@ -30,7 +30,7 @@ function FusionJS() {
 
 }
 
-FusionJS.prototype.settings  = 
+FusionJS.prototype.settings  =
 {
 	linker: {
 		gc_ttl: 5
@@ -49,28 +49,28 @@ FusionJS.prototype.Connect = function Connect(args) {
 
 	if(!args)
 		args = {server: FusionJS.prototype.settings.defaultArgs.server};
-	
+
 	var address = '127.0.0.1';
 	var port = 3030;
-	
+
 	if(args.server){
 		var splserver = args.server.split(':');
 		address = splserver[0];
-		
+
 		if(splserver.length>1)
 			port = Number(splserver[1]);
-		else 
-			if(isNumeric(address)){
-				port = address;
-				address = '127.0.0.1';
-			}
+		else
+		if(isNumeric(address)){
+			port = address;
+			address = '127.0.0.1';
+		}
 	}
-	
+
 	var serverLinker = new ServerLinker(address, port, this.settings);
-	
+
 	if(args.session)
 		return serverLinker.getSession(args.session);
-	
+
 	linkers.push(serverLinker);
 	return serverLinker;
 };
@@ -78,7 +78,7 @@ FusionJS.prototype.Connect = function Connect(args) {
 
 
 ///
-/// Garbage Collector 
+/// Garbage Collector
 ///
 function runGarbageCollector(){
 	debuglog("Running garbage collector");
@@ -119,12 +119,12 @@ var isFunction = function isFunction(functionToCheck) {
 
 var isNumeric = function isNumeric(input)
 {
-    return (input - 0) == input && (''+input).trim().length > 0;
+	return (input - 0) == input && (''+input).trim().length > 0;
 }
 
 function ServerLinker(HOST, PORT, settings){
 	var that = this;
-	
+
 	///
 	/// Client region
 	///
@@ -138,16 +138,16 @@ function ServerLinker(HOST, PORT, settings){
 		debuglog('Connected to Fusion.NET server: ' + HOST + ':' + PORT);
 		this.serverLinker.connected = true;
 	});
-	
+
 	// Add a 'data' event handler for the client socket
 	// data is what the server sent to this socket
 	/*this.client.on('data', function(data) {
-		//debuglog('DATA: ' + data);
-		// Close the client socket completely
-		//debuglog(data);
-		//this.destroy();
-		
-	});*/
+	 //debuglog('DATA: ' + data);
+	 // Close the client socket completely
+	 //debuglog(data);
+	 //this.destroy();
+
+	 });*/
 
 	// Add a 'close' event handler for the client socket
 	this.client.on('close', function() {
@@ -156,15 +156,16 @@ function ServerLinker(HOST, PORT, settings){
 		this.serverLinker.connected = false;
 
 	});
-	
+
 	this.client.on('end', function () {
-        // This may not been called since we are destroying the stream
-        // the first time 'data' event is received
-        //debuglog('All the data in the file has been read');
-        this.serverLinker.connected = false;
-        this.serverLinker.destroyed = true;
-    });
-	
+		// This may not been called since we are destroying the stream
+		// the first time 'data' event is received
+		// debuglog('All the data in the file has been read');
+		debuglog('End connection');
+		this.serverLinker.connected = false;
+		this.serverLinker.destroyed = true;
+	});
+
 	this.client.on('error', function (err) {
 		debuglog('error:', err);
 
@@ -173,72 +174,89 @@ function ServerLinker(HOST, PORT, settings){
 		else
 			this.destroyed = true;
 	});
-	
-	
+
+
 	///
 	///	Make requests (synchronous, wait for response if callback is undefined)
 	///
 	this.execRequest = function(args, callback){
-		this.lastRequest = 0;
+		try{
+			this.lastRequest = 0;
 
-		if(this.sessionId == -1 && args.request != "getSession")
-			this.getSession();
-		
-		var jsonStream = new JsonStream();
+			if(this.sessionId == -1 && args.request != "getSession")
+				this.getSession();
 
-		//Executing
-		var send = JSON.stringify(args);
-		debuglog('Sending: ' + send);
-		this.client.write(send);
-		
-		//Check data events 
-		var that = this;
-		var done = false;
-		var datares;
+			var jsonStream = new JsonStream();
 
-		var receiveData = function(data) {
-			debuglog('Receive: ' + data);
-			jsonStream.appendJson(data);
+			//Executing
+			var send = JSON.stringify(args);
 
-			if(jsonStream.isValid()){ //Json validated
-				if(isFunction(callback)) //If has callback
-					callback(JSON3.parse(jsonStream.json));
+			//Check data events
+			var that = this;
+			var done = false;
+			var datares;
 
-				done = true;
+			var receiveData = function(data) {
+				debuglog('Receive: ' + data);
+				jsonStream.appendJson(data);
+
+				if(jsonStream.isValid()){ //Json validated
+					if(isFunction(callback)){ //If has callback
+						that.lastRequest = getUnixTime();
+						var datares = JSON3.parse(jsonStream.json);
+
+						//Check exceptions
+						var err = null;
+						if(datares.response == 'exception')
+							err = new Error(datares.message);
+
+						setInterval(function() {
+							callback(datares, err);
+						}, 0);
+					}
+					done = true;
+				}
+				else
+					that.client.once('data', receiveData);
 			}
-			else 
-				that.client.once('data', receiveData);
+
+			debuglog('Sending: ' + send);
+			this.client.write(send);
+			this.client.once('data', receiveData);
+			if(callback === undefined){
+				deasync.loopWhile(function(){return !done;});
+
+				datares = JSON3.parse(jsonStream.json);
+
+				//Check exceptions
+				if(datares.response == 'exception')
+					throw new Error(datares.message);
+
+				this.lastRequest = getUnixTime();
+				return datares;
+			}
+
 		}
-
-		this.client.once('data', receiveData);
-		if(callback === undefined)
-			deasync.loopWhile(function(){return !done;});
-		
-		datares = JSON3.parse(jsonStream.json);
-
-		//Check exceptions
-		if(datares.response == 'exception')
-			throw new Error(datares.message);
-		
-		this.lastRequest = getUnixTime();
-		return datares;
+		catch(err){
+			throw err;
+		}
 	}
-	
+
 	///
 	/// Fast requests
 	///
 	this.getSession = function(parameters){
-		
+
 		var response = this.execRequest({ request: 'getSession', parameters: parameters});
-		
+
 		if(response.response == "reference")
 			this.sessionId = response.reference;
-		
+
 		debuglog("My session id is " + this.sessionId);
-		
+
 		return this.sessionId;
 	}
-	
+
 	this.get = function(variable){
 		if(settings.aliases[variable]!==undefined)
 			variable = settings.aliases[variable];
@@ -246,12 +264,12 @@ function ServerLinker(HOST, PORT, settings){
 		var response = that.execRequest({ request: 'get', variable: variable});
 		return that.varBoxToJObject(response);
 	}
-	
+
 	this.getType = function(type){ //Deprecated
 		return function(){
 			var args = this.argumentsToJsonArray(arguments);
 			var response = that.execRequest({ request: 'instance', type: type, arguments: args });
-			return that.varBoxToJObject(response);	
+			return that.varBoxToJObject(response);
 		}
 	}
 
@@ -261,7 +279,7 @@ function ServerLinker(HOST, PORT, settings){
 
 	this.argumentsToJsonArray = function(myargs){
 		var args = new Array();
-		
+
 		for(a=0; a<myargs.length; a++){
 			var arg = myargs[a];
 			var	oref;
@@ -270,22 +288,22 @@ function ServerLinker(HOST, PORT, settings){
 			else
 				args.push(this.parseJObject(arg));
 		}
-		
+
 		return args;
 	}
-	
+
 	this.varBoxToJObject = function(varbox){
 		if(varbox.response == 'varbox'){
-			switch(varbox.type){		
+			switch(varbox.type){
 				case 'valued':
-					return JSON.parse(varbox.object);		
+					return JSON.parse(varbox.object);
 				case 'ref':
 					return new ObjectWrapper(this, varbox.object, varbox);
 
 				case 'exception':
 					/*  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  **
-					**	      EXCEPTION HANDLING        **
-					**  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  */
+					 **	      EXCEPTION HANDLING        **
+					 **  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  */
 
 					//if(varbox.excode === 'UNDEFINED_MEMBER')
 					//	return undefined;
@@ -301,7 +319,7 @@ function ServerLinker(HOST, PORT, settings){
 							for(var s=0; s<sstack.length; s++){
 								var stack = sstack[s].replace(/\\/g, '/');
 
-								var tohide = 
+								var tohide =
 									stack.indexOf('node_modules/fusionjs') > -1 ||
 									stack.indexOf('node_modules/harmony-reflect') > -1;
 
@@ -330,7 +348,7 @@ function ServerLinker(HOST, PORT, settings){
 									var substr = str.substr(c).trim();
 									if(substr)
 										clrstack += '    ' + str.substr(c) + '\r\n';
-								}								
+								}
 							}
 
 							clrstack += "   === Colibri.NET Runtime ===";
@@ -344,61 +362,61 @@ function ServerLinker(HOST, PORT, settings){
 					throw error;
 			}
 		}
-		
+
 		return undefined;
 	}
-	
+
 	//In futuro si potrebbe approfondire il passaggio di variabili da js
 	this.parseJObject = function(obj){
 		return JSON.stringify(obj);
 	}
-	
-	this.objectHandling = function(ref, parameters){
-		return this.execRequest({request: 'object', ref: ref, parameters: parameters});
+
+	this.objectHandling = function(ref, parameters, callback){
+		return this.execRequest({request: 'object', ref: ref, parameters: parameters}, callback);
 	}
-	
+
 	this.objectGetPropertyType = function(ref, property){
 		//var res = this.execRequest({ request: 'object', ref: ref, operation: 'getPropertyType', property: property});
 		var res = this.objectHandling(ref, {command: 'getPropertyType', property: property});
-		
+
 		if(res.response == "propertyType")
 			return res.type;
-		
+
 		return "0";
 	}
-	
+
 	this.readResponseAsObject = function(response){
-		var resobj = null; 
-		
-		if(response.response == 'object'){	
+		var resobj = null;
+
+		if(response.response == 'object'){
 			switch(response.type){
 				case 'valued':
 					resobj = JSON.parse(response.object);
 					break;
-				
+
 				case 'ref':
 					resobj = new ObjectWrapper(this, response.object);
 					break;
 			}
 		}
-		
+
 		return resobj;
 	}
-	
+
 	this.endConnection = function(){
 		return this.execRequest({ request: 'endConnection' });
 	}
-	
-	
+
+
 	///
 	///	Linker region
 	///
 	this.destroyed = false;
 	this.end = function(){
-		//todo: Controllare che tutte le operazioni siano state terminate 
+		//todo: Controllare che tutte le operazioni siano state terminate
 		//con deasync.loopWhile(function(){return !done;});
-		
-		if(!this.destroyed){	
+
+		if(!this.destroyed){
 			if(this.connected){
 				this.endConnection();
 			}
@@ -409,17 +427,17 @@ function ServerLinker(HOST, PORT, settings){
 			debuglog("Client destroyed");
 		}
 	};
-	
-	
+
+
 	/// Exit events
 	function exitHandler(options, err) {
 
-		if (options.cleanup){ 
+		if (options.cleanup){
 			options.serverLinker.client.destroy();
 			debuglog('ServerLinker cleaned.');
 			process.exit();
 		}
-		
+
 		if (err) debuglog(err.stack);
 		if (options.exit) process.exit();
 	}
@@ -432,9 +450,9 @@ function ServerLinker(HOST, PORT, settings){
 
 	//catches uncaught exceptions
 	process.on('uncaughtException', exitHandler.bind(null, {serverLinker: this, exit:true}));
-	
-	
-	//Related classes 
+
+
+	//Related classes
 	function Request() {
 		this.Arguments = [];
 		this.IsSent = false;
@@ -453,34 +471,55 @@ function ServerLinker(HOST, PORT, settings){
 }
 
 
-function ObjectWrapper (serverLinker, ref, options) {	
+function ObjectWrapper (serverLinker, ref, options) {
 	if (isNaN(ref)) {
 		throw new TypeError('Invalid object reference');
 	}
-	
+
 	if(options == undefined)
 		options = {type: ''};
-	
-	var varBoxToJObject = function varBoxToJObject(varbox){
+
+	var varBoxToJObject = function varBoxToJObject(varbox, forceValue){
 		if(varbox.propertyType){
 			switch(varbox.propertyType){
 				case 'Method':
 					return function(){
-						var args = serverLinker.argumentsToJsonArray(arguments);
-						var response = serverLinker.objectHandling(ref, {command: 'methodExec', property: varbox.method, arguments: args});
-						return varBoxToJObject(response);
+						var args = Array.prototype.slice.call(arguments);
+
+						var userCallback = undefined, callback = undefined;
+						if(isFunction(args[args.length-1])){
+							userCallback = args[args.length-1];
+							args.pop();
+
+							callback = function(response, err){
+								if(!err)
+									response = varBoxToJObject(response, true)
+
+								userCallback(response, err);
+							}
+						}
+
+						args = serverLinker.argumentsToJsonArray(args);
+						var response = serverLinker.objectHandling(ref, {command: 'methodExec', property: varbox.method, arguments: args}, callback);
+
+						if(callback === undefined) {
+							return varBoxToJObject(response);
+						}
 					}
-					
+
 			}
 		}
-		
+
 		if(varbox.type == 'exception'){
 
 		}
 
-		return serverLinker.varBoxToJObject(varbox);
+		if(varbox.type == 'ref' && forceValue)
+			return serverLinker.varBoxToJObject(varbox).refAsValue();
+		else
+			return serverLinker.varBoxToJObject(varbox);
 	}
-	
+
 	//todo: Attenzione alla "false istanze"
 	var instanceType = function instanceType(args){
 		if(options.isType === "1"){
@@ -491,27 +530,27 @@ function ObjectWrapper (serverLinker, ref, options) {
 		else
 			throw new Error("You cannot instance this object, is not a type.");
 	}
-	
+
 	var proxyTarget = function() {
 		//console.log("function called!");
 	};
-	
+
 	/*
-		https://github.com/tvcutsem/harmony-reflect/blob/master/doc/traps.md
-		http://soft.vub.ac.be/~tvcutsem/invokedynamic/proxies_tutorial
-	*/
-	return new Proxy(proxyTarget, { 
-		get: function (target, name) {	
-			
+	 https://github.com/tvcutsem/harmony-reflect/blob/master/doc/traps.md
+	 http://soft.vub.ac.be/~tvcutsem/invokedynamic/proxies_tutorial
+	 */
+	return new Proxy(proxyTarget, {
+		get: function (target, name) {
+
 			if(options.isArray == "1" && name == "toString")
 				name = "valueOf";
 
 			switch(name){
 				case 'inspect':
-					/* 
-						C'è un utilizzo errato del comando inspect, che chiede solo di indicare gli elementi contenuti in un array 
-						https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
-					*/
+					/*
+					 C'è un utilizzo errato del comando inspect, che chiede solo di indicare gli elementi contenuti in un array
+					 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+					 */
 					var response = serverLinker.objectHandling(ref, {command: 'jsonSerialized'});
 					var valof = varBoxToJObject(response);
 					return JSON3.parse(valof);
@@ -540,19 +579,19 @@ function ObjectWrapper (serverLinker, ref, options) {
 			}
 
 
-		
+
 			var response = serverLinker.objectHandling(ref, {command: 'get', property: name});
 			return varBoxToJObject(response);
 		},
-		
+
 		set: function (target, name, val) {
 			var cmd = {command: 'set', property:name};
-			
+
 			if(val !== undefined){
 				var objref = -1;
 				if((objref = val.cjsGetObjectRef) === undefined)
 					cmd.val = serverLinker.parseJObject(val);
-				else 
+				else
 					cmd.objref = objref;
 			}
 			else
@@ -561,7 +600,7 @@ function ObjectWrapper (serverLinker, ref, options) {
 			var response = serverLinker.objectHandling(ref, cmd);
 			return varBoxToJObject(response);
 		},
-		
+
 		apply: function(target, wetThisArg, args) {
 			if(options.isType === "1")
 				return instanceType(args);
@@ -569,12 +608,12 @@ function ObjectWrapper (serverLinker, ref, options) {
 				debuglog(target + " ha provato ad eseguire ");
 				debuglog(wetThisArg);
 				debuglog(wetArgs);
-				
+
 				return 2;
 			}
-        },
-		
-		construct: function(target, args) {			
+		},
+
+		construct: function(target, args) {
 			return instanceType(args);
 		}
 
