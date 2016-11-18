354e51bdfc5eb37ee72ede8255d6323005dbf94f
LeoVPS <riccardo@leonardotravel.it>
2016-11-18 11:47:53 +0100

    CJS, motherfuckers!

--- index.js
+++ index.js
@@ -670,7 +670,7 @@ function ObjectWrapper (serverLinker, ref, options) {
 	}
 
 	var properties = varBoxToJObject(serverLinker.objectHandling(ref, {command: 'inspect'}));
-	properties.push("inspect", "valueOf", "toString", "refAsValue", "cjsDeepClone", "cjsGetObjectRef");
+	properties.push("inspect", "valueOf", "toString", "refAsValue", "cjsDeepClone", "cjsGetObjectRef", "cjsMotherfuckers");
 
 	var proxy = {};
 	for(var property of properties){
@@ -737,6 +737,9 @@ function ObjectWrapper (serverLinker, ref, options) {
 
 				case 'cjsGetObjectRef':
 					return ref;
+
+				case 'cjsMotherfuckers':
+					return true;
 			}
 
 
