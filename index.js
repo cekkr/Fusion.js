b250890981ecfb8f85ffc45f2b3b6d69d260ba58
LeoVPS <riccardo@leonardotravel.it>
2016-12-05 14:46:06 +0100

    Sorry for error ignoring

--- index.js
+++ index.js
@@ -803,10 +803,10 @@ function exitHandler(options, err) {
 }
 
 //do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
+//process.on('exit', exitHandler.bind(null,{cleanup:true}));
 
 //catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+//process.on('SIGINT', exitHandler.bind(null, {exit:true}));
 
 //catches uncaught exceptions
 /*process.on('uncaughtException', function(err){
